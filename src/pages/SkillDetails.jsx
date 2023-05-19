6ecd9af6b2520eb991728fd1ca6e96ad48f90501
HebaSakrH <128267375+HebaSakrH@users.noreply.github.com>
2023-05-20 01:30:32 +0300

    update/ delete skill

--- src/pages/SkillDetails.jsx
+++ src/pages/SkillDetails.jsx
@@ -1,8 +1,9 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 export default function SkillDetails() {
   const { skillid } = useParams();
+  const navigate = useNavigate()
   console.log(skillid);
   const [skill, setSkill] = useState();
 
@@ -27,18 +28,33 @@ export default function SkillDetails() {
     console.log(skill);
   }, [skill]);
 
+
+const handleDelete = async () => {
+  try {
+    const response = await fetch(`http://localhost:5005/skill/${skillid}`, {
+      method: 'DELETE',
+    })
+    if (response.status === 200) {
+      navigate('/')
+    }
+  } catch (error) {
+    console.log(error)
+    
+  }
+}
+
   return (
     <>
       {skill ? (
         <div>
-        
           <h1>Details of {skill.title}</h1>
           <h2>{skill.details}</h2>
+          <Link to={`/update/${skillid}`}> Update </Link>
+         <button type="button" onClick={handleDelete}> Delete </button>
         </div>
       ) : (
         <h3>Loading...</h3>
       )}
     </>
   );
-    
 }
