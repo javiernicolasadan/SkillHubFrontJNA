ebbd530a99ec19561980f0137f9a4ae668954e11
Elisa Exposito <eliexme@gmail.com>
2023-05-19 14:25:26 +0100

    all pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Profile from './pages/Profile'
 import PrivateRoute from './components/PrivateRoute'
 import AddSkill from './pages/AddSkill'
 import AddEvent from './pages/AddEvent'
+import SkillDetails from './pages/SkillDetails'
+import EventDetails from './pages/EventDetails'
+import AllSkills from './pages/AllSkills'
 
 function App() {
 
@@ -20,8 +23,12 @@ function App() {
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/profile' element={<PrivateRoute><Profile/></PrivateRoute>}/>
-        <Route path='/addskill' element={<AddSkill/>} />
-        <Route path='/addevent' element={<AddEvent/>}/>
+        <Route path='/allskills' element={<AllSkills/>} />
+        <Route path='/addskill' element={<PrivateRoute><AddSkill/></PrivateRoute>} />
+        <Route path='/addevent' element={<PrivateRoute><AddEvent/></PrivateRoute>}/>
+        <Route path='/skilldets/:skillid' element={<PrivateRoute><SkillDetails/></PrivateRoute>}/>
+        <Route path='/eventdets/:eventid' element={<PrivateRoute><EventDetails/></PrivateRoute>}/>
+
         
       </Routes>
     </>
---
+++ src/pages/AllSkills.jsx
@@ -0,0 +1,5 @@
+export default function AllSkills() {
+  return (
+    <div>AllSkills</div>
+  )
+}
