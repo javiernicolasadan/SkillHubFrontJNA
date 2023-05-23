845329b41190fa2e5d6d58e633309168fc250ff6
HebaSakrH <128267375+HebaSakrH@users.noreply.github.com>
2023-05-23 12:33:40 +0300

    update done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,14 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { SessionContext } from "../contexts/SessionContext";
 
-
 export default function Navbar() {
-
-  const {isLoggedIn} = useContext(SessionContext)
-
+  const { isLoggedIn } = useContext(SessionContext);
 
   return (
     <nav>
-    <Link to={'/'}>Home</Link>
+      <Link to={"/"}>Home</Link>
       {isLoggedIn && <Link to="/profile">Profile</Link>}
-      {isLoggedIn && <Link to="/allskills">All skills</Link>}
+      {isLoggedIn && <Link to="/allskills">Skills Venue</Link>}
     </nav>
-  )
+  );
 }
