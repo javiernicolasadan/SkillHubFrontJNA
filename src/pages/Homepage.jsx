070d1869c18480c16537a813c38e4f33e540ce55
Elisa Exposito <eliexme@gmail.com>
2023-06-05 21:15:34 +0100

    shortened descr

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -10,14 +10,14 @@ export default function Homepage() {
     <>
       <header>
         <img className="neonLogo" src="/images/logo white.png"/>
-        <p>Welcome to SkillHub, the ultimate platform for expanding your knowledge and mastering new skills. Whether you're an aspiring professional, a lifelong learner, or simply curious about the world, SkillHub offers a diverse range of resources to help you reach your full potential. Let's dive in together and embrace the power of knowledge!
+        <p>Welcome to SkillHub, the ultimate platform for expanding knowledge and mastering new skills. Discover endless possibilities as an aspiring professional, lifelong learner, or curious explorer. Let's dive into the power of knowledge together!
         </p>        
       </header>
 
       <section id="about">
         <h2>ABOUT</h2>
         <p>
-        SkillHub is a global platform dedicated to accessible learning for all. With diverse courses for all skill levels, expert instructors, and interactive experiences, we create a comprehensive and enjoyable learning environment. Join us now to unlock knowledge, excel in your career, and broaden your horizons. Let SkillHub be your trusted companion on your learning journey. Together, we can achieve greatness!
+        SkillHub is a global platform dedicated to accessible learning for everyone. With diverse courses, expert instructors, and interactive experiences, we foster comprehensive and enjoyable learning environments. Join us now to unlock knowledge, excel in your career, and broaden your horizons. Let SkillHub be your trusted companion on the journey to greatness!
         </p>
 
         {!isLoggedIn &&
