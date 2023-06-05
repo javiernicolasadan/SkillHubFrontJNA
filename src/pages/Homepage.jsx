88c9b69d8b07dc174b1d115475ed37f43e963926
Elisa Exposito <eliexme@gmail.com>
2023-06-05 21:05:08 +0100

    description test length

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -10,11 +10,16 @@ export default function Homepage() {
     <>
       <header>
         <img className="neonLogo" src="/images/logo white.png"/>
-        <p>At Skill HuB, we believe that everyone <br/>
-        has a unique set of skills and knowledge that can be shared with others.<br/>
-        Our platform is designed to connect individuals with different skill sets and<br/>
-        interests, creating a community where learning and personal growth are prioritized.<br/><br/>
+        <p>Welcome to SkillHub, the ultimate platform for expanding your knowledge and mastering new skills. Whether you're an aspiring professional, a lifelong learner, or simply curious about the world, SkillHub offers a diverse range of resources to help you reach your full potential. Let's dive in together and embrace the power of knowledge!
+        </p>        
+      </header>
+
+      <section id="about">
+        <h2>ABOUT</h2>
+        <p>
+        SkillHub is a global platform dedicated to accessible learning for all. With diverse courses for all skill levels, expert instructors, and interactive experiences, we create a comprehensive and enjoyable learning environment. Join us now to unlock knowledge, excel in your career, and broaden your horizons. Let SkillHub be your trusted companion on your learning journey. Together, we can achieve greatness!
         </p>
+
         {!isLoggedIn &&
         <div>
           <p  style={{ fontSize: '1.3rem', fontWeight: 500 }}>Join us now!</p>
@@ -28,8 +33,8 @@ export default function Homepage() {
           <p  style={{ fontSize: '1.3rem', fontWeight: 500 }}>Check our info out below!</p>
         </div>
         }
-        
-      </header>
+
+      </section>
 
       <div className="allLink allskills">
         <Link to={'/allskills'} >All Skills</Link>
